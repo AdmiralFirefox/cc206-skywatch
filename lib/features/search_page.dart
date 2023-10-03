5cb31b1b45df80fd697824740899f32b0ccbe80e
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-10-03 13:57:27 +0800

    Added search page skeleton with widget list

---
+++ lib/features/search_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SearchPage());
+}
+
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // Text(), RichText(),TextField(), Autocomplete(),
+          // RaisedButton(), Icon()
+        ],
+      ),
+    );
+  }
+}
