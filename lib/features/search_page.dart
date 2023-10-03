309b31b1e0460e73f0b86cc2bdb16df0318a15c9
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-10-03 14:46:21 +0800

    Added widgets on search page

--- lib/features/search_page.dart
+++ lib/features/search_page.dart
@@ -10,11 +10,55 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          // Text(), RichText(),TextField(), Autocomplete(),
-          // RaisedButton(), Icon()
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            const Icon(
+              Icons.house,
+              color: Colors.black,
+              size: 40.0,
+              semanticLabel: 'Text to announce in accessibility modes',
+            ),
+            const Text(
+              "SkyWatch",
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            RichText(
+              text: const TextSpan(
+                text: "Search for a City or Country",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Form(
+              autovalidateMode: AutovalidateMode.always,
+              child: TextFormField(
+                onSaved: (String? value) {},
+              ),
+            ),
+            ElevatedButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.deepOrange,
+              ),
+              onPressed: () {
+                print("Searching...");
+              },
+              child: const Text(
+                'Search Country',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
