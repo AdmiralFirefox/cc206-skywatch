3c2c6452e41aa8ccb217e2acfc78e69298320bb5
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-11-14 03:09:11 +0800

    Adjusted code structure on search form

--- lib/features/search_page.dart
+++ lib/features/search_page.dart
@@ -1,4 +1,3 @@
-import 'package:cc206_skywatch/components/search/search_form.dart';
 import 'package:flutter/material.dart';
 
 class SearchPage extends StatefulWidget {
@@ -11,6 +10,13 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   TextEditingController textController = TextEditingController();
   String submittedText = "";
+
+  final List<String> _options = const <String>[
+    'New York',
+    'Tokyo',
+    'Manila',
+    'Seoul'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -34,20 +40,116 @@ class _SearchPageState extends State<SearchPage> {
           ),
           child: Column(
             children: [
-              SearchForm(
-                textController: textController,
-                handleOnTap: () {
-                  setState(() {
-                    submittedText = textController.text;
-                    textController.text = "";
-                  });
-                },
-                handleOnSubmit: (String value) {
-                  setState(() {
-                    submittedText = value;
-                    textController.text = "";
-                  });
-                },
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                child: LayoutBuilder(builder:
+                    (BuildContext context, BoxConstraints constraints) {
+                  return Autocomplete<String>(
+                    optionsBuilder: (TextEditingValue textEditingValue) {
+                      if (textEditingValue.text == '') {
+                        return const Iterable<String>.empty();
+                      }
+                      return _options.where((option) => option
+                          .toLowerCase()
+                          .contains(textEditingValue.text.toLowerCase()));
+                    },
+                    optionsViewBuilder: (BuildContext context,
+                        AutocompleteOnSelected<String> onSelected,
+                        Iterable<String> options) {
+                      return Align(
+                        alignment: Alignment.topLeft,
+                        child: Material(
+                          color: Colors.white, // Autocomplete Background color
+                          elevation: 4.0,
+                          child: SizedBox(
+                            width: constraints.maxWidth,
+                            child: ListView.builder(
+                              padding: EdgeInsets.zero,
+                              itemCount: options.length,
+                              shrinkWrap: true,
+                              itemBuilder: (BuildContext context, int index) {
+                                final String option = options.elementAt(index);
+                                return InkWell(
+                                  onTap: () => onSelected(option),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(16.0),
+                                    child: Text(
+                                      option,
+                                      style: const TextStyle(
+                                        // Autocomplete Text Color
+                                        color: Colors.black,
+                                        fontFamily: "Poppins",
+                                        fontWeight: FontWeight.w400,
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    onSelected: (String selection) {
+                      setState(() {
+                        submittedText = selection;
+                        FocusManager.instance.primaryFocus?.unfocus();
+                      });
+                    },
+                    fieldViewBuilder: (BuildContext context,
+                        TextEditingController textEditingController,
+                        FocusNode focusNode,
+                        VoidCallback onFieldSubmitted) {
+                      return TextFormField(
+                        controller: textEditingController,
+                        focusNode: focusNode,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: const BorderSide(
+                              color: Colors.transparent,
+                              width: 2.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            borderSide: const BorderSide(
+                              color: Colors.deepOrangeAccent,
+                              width: 2.5,
+                            ),
+                          ),
+                          hintText: "Search for a City/Country",
+                          hintStyle: const TextStyle(
+                            color: Colors.black,
+                            fontFamily: "Poppins",
+                            fontWeight: FontWeight.w400,
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                submittedText = textEditingController.text;
+                                textEditingController.text = "";
+                              });
+                              focusNode.unfocus();
+                            },
+                            child: const Icon(Icons.search),
+                          ),
+                        ),
+                        textInputAction: TextInputAction.done, // Set it to done
+                        // Handle the submission when "Done" is pressed
+                        onFieldSubmitted: (value) {
+                          setState(() {
+                            submittedText = value;
+                            textEditingController.text = "";
+                          });
+                          focusNode.unfocus();
+                        },
+                      );
+                    },
+                  );
+                }),
               ),
               Text(
                 submittedText,
