2972d966dfb4bc22069f649da41575255eb82e07
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-11-19 23:37:00 +0800

    Added empty list state on bookmarks

--- lib/components/bookmarks_drawer.dart
+++ lib/components/bookmarks_drawer.dart
@@ -16,6 +16,127 @@ class BookmarksDrawer extends StatefulWidget {
 }
 
 class _BookmarksDrawerState extends State<BookmarksDrawer> {
+  Widget _emptyListState() {
+    return ListView(
+      padding: EdgeInsets.zero,
+      children: const [
+        SizedBox(
+          height: 120.0,
+          child: DrawerHeader(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(24, 66, 90, 1),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.favorite,
+                  color: Colors.white,
+                  size: 25.0,
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  "Bookmarks",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        ListTile(
+          title: Text(
+            "No bookmarked place",
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: "Poppins",
+              fontWeight: FontWeight.w600,
+              fontSize: 16.0,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _bookmarksContent() {
+    return ListView(
+      padding: EdgeInsets.zero,
+      children: [
+        const SizedBox(
+          height: 120.0,
+          child: DrawerHeader(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(24, 66, 90, 1),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.favorite,
+                  color: Colors.white,
+                  size: 25.0,
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  "Bookmarks",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        ...widget.favoritePlaces.reversed.map((favoritePlace) {
+          return ListTile(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Text(
+                    favoritePlace.placeName,
+                    softWrap: true,
+                    overflow: TextOverflow.visible,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16.5,
+                      height: 1.3,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                GestureDetector(
+                  child: const Icon(
+                    Icons.favorite,
+                    color: Color.fromRGBO(231, 84, 128, 1),
+                    size: 28.0,
+                  ),
+                  onTap: () {
+                    widget.onFavoritePlaceRemoved(favoritePlace);
+                  },
+                ),
+              ],
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          );
+        }),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -25,77 +146,9 @@ class _BookmarksDrawerState extends State<BookmarksDrawer> {
           bottomRight: Radius.circular(0),
         ),
       ),
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          const SizedBox(
-            height: 120.0,
-            child: DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(24, 66, 90, 1),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.favorite,
-                    color: Colors.white,
-                    size: 25.0,
-                  ),
-                  SizedBox(width: 10.0),
-                  Text(
-                    "Bookmarks",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: "Poppins",
-                      fontWeight: FontWeight.w600,
-                      fontSize: 15.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          ...widget.favoritePlaces.reversed.map((favoritePlace) {
-            return ListTile(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Text(
-                      favoritePlace.placeName,
-                      softWrap: true,
-                      overflow: TextOverflow.visible,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontFamily: "Poppins",
-                        fontWeight: FontWeight.w600,
-                        fontSize: 16.5,
-                        height: 1.3,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 10),
-                  GestureDetector(
-                    child: const Icon(
-                      Icons.favorite,
-                      color: Color.fromRGBO(231, 84, 128, 1),
-                      size: 28.0,
-                    ),
-                    onTap: () {
-                      widget.onFavoritePlaceRemoved(favoritePlace);
-                    },
-                  ),
-                ],
-              ),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            );
-          }),
-        ],
-      ),
+      child: widget.favoritePlaces.isEmpty
+          ? _emptyListState()
+          : _bookmarksContent(),
     );
   }
 }
