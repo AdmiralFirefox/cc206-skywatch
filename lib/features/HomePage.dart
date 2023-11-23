a601aae8a49937b8ee267f9b4d46611410045d74
Hjmontalban <humprheyjohn.montalban@wvsu.edu.ph>
2023-11-23 23:52:58 +0800

    added styling on homepage

--- lib/features/HomePage.dart
+++ lib/features/HomePage.dart
@@ -6,57 +6,83 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            left: 45,
-            top: 50,
-            right: 45,
-            bottom: 20,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/main-background.jpg'),
+            fit: BoxFit.cover,
           ),
-          child: Column(
-            children: [
-              const Icon(
-                Icons.cloud_sharp,
-                color: Color.fromARGB(255, 84, 157, 175),
-                size: 60.0,
-                semanticLabel: 'cloud',
-              ),
-              const Text(
-                "SkyWatch: Global Weather Monitoring System \n",
-                style: TextStyle(
-                  fontSize: 45.0,
-                  fontWeight: FontWeight.bold,
+        ),
+        child: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(94, 121, 122, 0.7),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            padding: const EdgeInsets.all(25.0),
+            margin: const EdgeInsets.only(
+              left: 45.0,
+              top: 40.0,
+              right: 45.0,
+              bottom: 40.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  ('assets/images/web-logo.png'),
+                  width: 50.0,
+                  height: 50.0,
                 ),
-              ),
-              RichText(
-                text: const TextSpan(
-                  text:
-                      "A weather application to know the weather of a specific place, either be a city or country.",
+                const SizedBox(
+                  height: 20.0,
+                ),
+                const Text(
+                  "SkyWatch: Global Weather Monitoring System \n",
+                  textAlign: TextAlign.center,
                   style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors
-                      .deepOrange, // Use 'primary' for button background color
-                ),
-                onPressed: () {
-                  print("Searching...");
-                },
-                child: const Text(
-                  'Start Searching',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
                     color: Colors.white,
+                    fontSize: 15.5,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ],
+                const Text(
+                  "A weather application to know the weather of a specific place, either be a city or country.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 13.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w500,
+                      fontSize: 13.0,
+                    ),
+                    backgroundColor: Color.fromRGBO(252, 96, 66, 1),
+                  ),
+                  onPressed: () {
+                    print("Searching...");
+                  },
+                  child: const Text(
+                    'Start Searching',
+                    style: TextStyle(
+                      fontFamily: "Poppins",
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
