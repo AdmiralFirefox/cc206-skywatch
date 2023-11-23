63b14af9e269479b5e2db38ff211dc3f98c0129f
Hjmontalban <humprheyjohn.montalban@wvsu.edu.ph>
2023-11-24 01:03:07 +0800

    added TabController on Homepage for TabNavigation

--- lib/features/HomePage.dart
+++ lib/features/HomePage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key); // Fix the constructor
+  final VoidCallback onButtonPressed;
+
+  const HomePage({Key? key, required this.onButtonPressed}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -68,7 +70,7 @@ class HomePage extends StatelessWidget {
                   backgroundColor: const Color.fromRGBO(252, 96, 66, 1),
                 ),
                 onPressed: () {
-                  print("Searching...");
+                  onButtonPressed();
                 },
                 child: const Text(
                   'Start Searching',
