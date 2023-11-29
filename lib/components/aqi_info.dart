beb50b522347b44eb53142c3c8f7fd8a6e67278d
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-11-29 17:47:00 +0800

    Adjusted structure of aqi section

--- lib/components/aqi_info.dart
+++ lib/components/aqi_info.dart
@@ -11,52 +11,105 @@ class AQIInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color.fromRGBO(218, 243, 247, 1),
-        borderRadius: BorderRadius.circular(5.0),
-      ),
-      padding: const EdgeInsets.all(5.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text(
-            "AQI",
-            style: TextStyle(
-              color: Colors.black,
-              fontFamily: "Poppins",
-              fontWeight: FontWeight.w700,
-              fontSize: 16.5,
+    return FutureBuilder<Map<String, dynamic>>(
+      future: weatherAQIFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(218, 243, 247, 1),
+              borderRadius: BorderRadius.circular(5.0),
             ),
-          ),
-          const SizedBox(height: 5.0),
-          FutureBuilder<Map<String, dynamic>>(
-            future: weatherAQIFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const SpinKitThreeBounce(
+            padding: const EdgeInsets.all(5.0),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "AQI",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16.5,
+                  ),
+                ),
+                SizedBox(height: 5.0),
+                SpinKitThreeBounce(
                   color: Colors.black,
                   size: 25.0,
-                );
-              } else if (snapshot.hasError) {
-                return const Text("-----");
-              } else {
-                var data = snapshot.data;
+                )
+              ],
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Container(
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(218, 243, 247, 1),
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            padding: const EdgeInsets.all(5.0),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "AQI",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16.5,
+                  ),
+                ),
+                SizedBox(height: 2.0),
+                Text(
+                  "----",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: "Poppins",
+                    fontSize: 14.0,
+                  ),
+                ),
+              ],
+            ),
+          );
+        } else {
+          var data = snapshot.data;
 
-                return Text(
+          return Container(
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(218, 243, 247, 1),
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            padding: const EdgeInsets.all(5.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  "AQI",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: "Poppins",
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16.5,
+                  ),
+                ),
+                const SizedBox(height: 5.0),
+                Text(
                   data!["list"][0]["main"]["aqi"].toString(),
                   style: const TextStyle(
                     color: Colors.black,
                     fontFamily: "Poppins",
                     fontSize: 14.0,
                   ),
-                );
-              }
-            },
-          )
-        ],
-      ),
+                )
+              ],
+            ),
+          );
+        }
+      },
     );
   }
 }
