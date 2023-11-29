ef7d612842de46c5e5a0062b89fce8797a0b058c
AdmiralFirefox <jjlcuadradev@gmail.com>
2023-11-29 17:32:33 +0800

    Added styling on loading and error states on aqi

--- lib/components/aqi_info.dart
+++ lib/components/aqi_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class AQIInfo extends StatelessWidget {
   final Future<Map<String, dynamic>> weatherAQIFuture;
@@ -29,17 +30,28 @@ class AQIInfo extends StatelessWidget {
               fontSize: 16.5,
             ),
           ),
+          const SizedBox(height: 5.0),
           FutureBuilder<Map<String, dynamic>>(
             future: weatherAQIFuture,
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Text("Loading");
+                return const SpinKitThreeBounce(
+                  color: Colors.black,
+                  size: 25.0,
+                );
               } else if (snapshot.hasError) {
-                return const Text("Error");
+                return const Text("-----");
               } else {
                 var data = snapshot.data;
 
-                return Text(data!["list"][0]["main"]["aqi"].toString());
+                return Text(
+                  data!["list"][0]["main"]["aqi"].toString(),
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontFamily: "Poppins",
+                    fontSize: 14.0,
+                  ),
+                );
               }
             },
           )
